b8b972891d7a262f38228dc50ece9d0a2f42a459
v_rubashko <v_rubashko@wargaming.net>
2024-05-01 14:54:32 +0300

    add default chrome

--- test_items.py
+++ test_items.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import time
+from selenium.webdriver.common.by import By
+
+def test_add_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(5)
+    add_button = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")
+    assert len(add_button) > 0, 'Button "Add to basket" is not found'
